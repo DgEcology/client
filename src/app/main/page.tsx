bd7fccb645c15b858690c717aa693eebfc93b26e
ArtAndDmi <art.and.dmi@yandex.ru>
2024-09-18 09:28:10 +0300

    initial

---
+++ src/app/main/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './main.module.css'
+
+const Page = () => {
+    const events = {
+
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Page

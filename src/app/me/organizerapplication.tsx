28d83c3a8be035d1d30264fafa233b5a3489f63c
Konstantin Zhigaylo <zero@kostyazero.com>
2024-09-18 16:26:20 +0300

    feat: organizer application

--- src/app/me/organizerapplication.tsx
+++ src/app/me/organizerapplication.tsx
@@ -11,31 +11,54 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { FormEvent, useState } from "react";
 
 export default function OrganizerApplication() {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className="w-full">Подать заявку</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Подача заявки</DialogTitle>
-          <DialogDescription>
-            Заполните форму для подачи заявки на становление организатором.
-          </DialogDescription>
-        </DialogHeader>
-        <div>
-          
+  const [file, setFile] = useState<File | undefined>();
+  const [open, setOpen] = useState(false);
 
-        </div>
-        <DialogFooter>
-          <Button type="submit">Отправить</Button>
-          <DialogClose asChild>
-            <Button type="button">Закрыть</Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    // TODO: Create application on website
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button className="w-full">Подать заявку</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Подача заявки</DialogTitle>
+            <DialogDescription>
+              Заполните форму для подачи заявки на становление организатором.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
+              <p className="text-xl font-bold">Название организации</p>
+              <Input placeholder="1234" type="text" name="organization" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className="text-xl font-bold">Номер организации (ОГРН)</p>
+              <Input placeholder="1234" type="text" name="serial" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className="text-xl font-bold">Об организации</p>
+              <Input placeholder="1234" type="text" name="about" />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Отправить</Button>
+            <DialogClose asChild>
+              <Button type="button">Закрыть</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </form>
   );
 }

230fd4e5a060e8130d2f7546dc06dc3f4c8eafb1
Konstantin Zhigaylo <zero@kostyazero.com>
2024-09-19 08:58:41 +0300

    feat: return main page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,11 +1,9 @@
 import EditForm from "./editform";
-import Header from "@/components/header/header";
 import OrganizerForm from "./organizerform";
 
 export default async function Me() {
   return (
     <main className="z-10 box-border flex">
-      <Header />
       <div className="max-w-[900px] w-full p-4 mt-[80px] mx-auto">
         <OrganizerForm />
         <EditForm />
